12a4f2921ae5864445f3820a0c6bec2f550c90b5
Tyler M <92123605+Tyler-Zorb@users.noreply.github.com>
2022-12-08 16:53:04 -0600

    After clicking a repo, you'll now see info about it

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 const username = "Tyler-Zorb";       //this adds my GitHub username.
 const repoList = document.querySelector(".repo-list");      //This variable will select the unordered list to display the repos list.
+const allReposContainer = document.querySelector(".repos");   //This variable selects the section with a class of "repos" where all my repo info appears.
+const repoData = document.querySelector(".repo-data");    //This variable selects the section with a calss of "repo-data" where the individual repo data will appear.
 
 const gitUserInfo = async function () {         //this is an async function to fetch info from my GitHub profile using the GitHub API address. I had to target the "users" endpoint and use a template literal to add the global username variable to the endpoint.
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -43,4 +45,44 @@ const displayRepos = function (repos) {     //This function displays info about 
        repoItem.innerHTML = `<h3>${repo.name}</h3> `;       //This gives each item an <h3> element with the repo name
        repoList.append(repoItem);       //Here I am appending the list item to the global variable that selects the unordered repos list. So appending the repoItem to the global var of repoList.
     }
+};
+
+repoList.addEventListener("click", function (e) {   //Here's an event listener for a click event on the unordered list with a class of "repo-list". Passing the event (e) in the callback function.
+  if (e.target.matches("h3")) {           //This conditional statement checks if the event target(i.e. the element that was clicked on) matches the <h3> element (i.e. the name of the repo)(they gave me this line becuase i haven't used matches before)
+    const repoName = e.target.innerText;      //This variable targets the innerText where the event happens.
+    getRepoInfo(repoName);      //calling the getRepoInfo function with repoName as an argument.
+  }         
+});
+
+const getRepoInfo = async function (repoName) {     //async function that accepts repoName as a parameter. This function gets specfic repo information.
+  const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);      //fetch request to grab info about the specific repository.
+  const repoInfo = await fetchInfo.json();      //declared a var to resolve and save the json response.
+  console.log(repoInfo);    //logging out repoInfo.
+  
+  const fetchLanguages = await fetch(repoInfo.languages_url);     //This variable will fetch data from language_url property of my repoInfo.
+  const languageData = await fetchLanguages.json();   //This variable will save the json response.
+  //console.log(languageData);    commenting this out, it was just to see if the console would display the languages used when click on a repo and it does.
+  
+  const languages = [];   //This is an empty array for the languages.
+  for (const language in languageData) {       //this for in loop will loop thru the languages array from the languageData object.
+    languages.push(language);
+  }
+  //console.log(languages);   commenting this out. It was just to see if this would show the array with the languages used in the console and it does.
+  displayRepoInfo(repoInfo, languages);   //calling the displayRepoInfo function I made below this. And passing it the repoInfo object and languages array 
+};
+
+const displayRepoInfo = function (repoInfo, languages) {
+  repoData.innerHTML = "";    //This will empty the HTML of the section with a class of "repo-data" where the individual repo data will appear.
+  repoData.classList.remove("hide");  //Here I'm unhiding or show the repo-data element.
+  allReposContainer.classList.add("hide");  //Here I'm hiding the element with the class of "repos"
+  const div = document.createElement("div");  //creating a new div here like above. But here I'm adding the selected repository's name, description, default branch, and link to its code on github.
+  //Here where I'm creating the div I have to use the json data in the 5 palceholders to grab the relevant properties to display on the page. I'm using the properties from the object I retrieved when I fetched the specific repos.
+  div.innerHTML = `           
+  <h3>Name: ${repoInfo.name}</h3>
+  <p>Description: ${repoInfo.description}</p>
+  <p>Default Branch: ${repoInfo.default_branch}</p>
+  <p>Languages: ${languages.join(", ")}</p>
+  <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+  `;
+  repoData.append(div);     //appending the new div element to the section with a class of "repo-data"
 };
